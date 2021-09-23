18501a0de56d00463d7059966a45a167e82389ee
Dustin <dustinibrao@gmail.com>
2021-09-22 16:00:48 -1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="grid-container">
+			<header className="row">
+				<div>
+					<a className="brand" href="index.html">
+						Lolas Snack Shack
+					</a>
+				</div>
+				<div>
+					<a href="cart.html">Cart</a>
+					<a href="signIn.html">Sign In</a>
+				</div>
+			</header>
+			<main>
+				<div className="row center">
+					<div className="card">
+						<a href="producthtml">
+							<img
+								className="medium"
+								src="./images/beefliver.png"
+								alt="product"
+							/>
+						</a>
+						<div className="card-body">
+							<a href="product.html">
+								<h2>Dried Beef Liver</h2>
+							</a>
+							<div className="rating">
+								<span>
+									<i className="fa fa-star"></i>
+								</span>
+								<span>
+									<i className="fa fa-star"></i>
+								</span>
+								<span>
+									<i className="fa fa-star"></i>
+								</span>
+								<span>
+									<i className="fa fa-star"></i>
+								</span>
+								<span>
+									<i className="fa fa-star"></i>
+								</span>
+							</div>
+							<div className="price">$20</div>
+						</div>
+					</div>
+					<div className="card">
+						<a href="producthtml">
+							<img
+								className="medium"
+								src="./images/beefliver.png"
+								alt="product"
+							/>
+						</a>
+						<div className="card-body">
+							<a href="product.html">
+								<h2>Dried Beef Liver</h2>
+							</a>
+							<div className="rating">
+								<span>
+									<i className="fa fa-star"></i>
+								</span>
+								<span>
+									<i className="fa fa-star"></i>
+								</span>
+								<span>
+									<i className="fa fa-star"></i>
+								</span>
+								<span>
+									<i className="fa fa-star"></i>
+								</span>
+								<span>
+									<i className="fa fa-star"></i>
+								</span>
+							</div>
+							<div className="price">$20</div>
+						</div>
+					</div>
+					<div className="card">
+						<a href="producthtml">
+							<img
+								className="medium"
+								src="./images/beefliver.png"
+								alt="product"
+							/>
+						</a>
+						<div className="card-body">
+							<a href="product.html">
+								<h2>Dried Beef Liver</h2>
+							</a>
+							<div className="rating">
+								<span>
+									<i className="fa fa-star"></i>
+								</span>
+								<span>
+									<i className="fa fa-star"></i>
+								</span>
+								<span>
+									<i className="fa fa-star"></i>
+								</span>
+								<span>
+									<i className="fa fa-star"></i>
+								</span>
+								<span>
+									<i className="fa fa-star"></i>
+								</span>
+							</div>
+							<div className="price">$20</div>
+						</div>
+					</div>
+				</div>
+			</main>
+			<footer className="row center">All rights reserved</footer>
+		</div>
+	);
 }
 
 export default App;
